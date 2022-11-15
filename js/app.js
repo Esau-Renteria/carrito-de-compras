aef7178152d349635a673fb0baecf45ec96086ee
Esau <esaurenteria.isc@gmail.com>
2022-11-15 11:51:40 -0600

    Mostrando los cursos en el carrito

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ const vaciarCarrito = document.querySelector('#vaciar-carrito');
 
 const listaCursos = document.querySelector('#lista-cursos');
 
+let articulosCarrito = [];
+
 cargaEventListeners();
 
 function cargaEventListeners(){
@@ -43,6 +45,76 @@ function leerDatosCurso(curso){
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
     }
-    console.log(infoCurso);
 
+
+    //Revisa si un elemento ya existe en el carrito
+    const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
+
+    if(existe){
+        //Actualizamos la cantidad
+        const cursos = articulosCarrito.map( curso => {
+            if(curso.id === infoCurso.id){
+                curso.cantidad++;
+                return curso; //retorna el objeto actualizado
+            }else{
+                return curso;//retorma los objetos que no estan duplicados
+            }
+
+        });
+        articulosCarrito = [...cursos];
+    }else{
+        //Agregamos el curso al carrito
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+  
+
+
+    //Agregar elementos al arreglo de carrito
+
+   
+
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+
+//Muestra el carrito de compras en el HTML
+
+function carritoHTML(){
+
+
+    //Limpiar el HTML
+    limpiarHTML();
+
+
+
+
+    //Recorre el carrito
+    articulosCarrito.forEach(curso =>{
+
+        const { imagen, titulo, precio, cantidad, id} = curso;
+        const row = document.createElement('tr');
+        row.innerHTML =`
+        <td><img src="${imagen}" width="100"></td>
+        <td> ${titulo}</td>
+        <td>${precio}</td>
+        <td>${cantidad}</td>
+        <td> <a href="#" class="borrar-curso" data-id="${id}"> X </a></td>
+        `;
+
+        //Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    } )
+}
+
+
+//Eliminar los cursos del tbody
+
+function limpiarHTML(){
+
+
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
